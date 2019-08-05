f062befbeb5fd488a70d9ba5799410e0918cb1ea
Eivind Alexander Bergem <eivind.bergem@gmail.com>
2019-08-05 21:46:38 +0200

    Initial commit

---
+++ ara-dates.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+months = {}
+for line in open("months.txt"):
+    parts = line.split()
+    months[parts[1].lower()] = parts[2]
+
+weekdays = {}
+
+for line in open("weekdays.txt"):
+    parts = line.split()
+    weekdays[parts[0].lower()] = parts[2]
+
+regex = re.compile("^(\d+)\.([^ ]+) \(([^)]*)\).*$")
+
+for line in open("dates.txt"):
+    matches = regex.match(line)
+
+    if matches:
+        date, month, day = matches.groups()
+
+        print "%s %s %s" % (weekdays[day], date, months[month])
+
